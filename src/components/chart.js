6c87b28a59204b9f11e4a312db9516d40f16c10d
danielosborne <nycosborne@gmail.com>
2018-10-15 14:33:20 -0400

    cleaned code added postMost fun

--- src/components/chart.js
+++ src/components/chart.js
@@ -10,13 +10,13 @@ import { fetchWpData } from '../actions';
 class Chart extends Component {
   constructor(props){
     super(props);
-    this.calc = this.calc.bind(this);
+    this.returnGraph = this.returnGraph.bind(this);
   }
   componentDidMount(){
     this.props.fetchWpData();
   }
 
-  convertDate(dateAsString){
+  convertDatePerMonth(dateAsString){
     const epocArray = [];
     const postRate = [0];
     const oneMonth =  2592000000;
@@ -34,55 +34,46 @@ class Chart extends Component {
     for (var i = 0; i < arrayLeng - 1; i++) {
         arrayLimiter[i+1] = (arrayLimiter[i] + oneMonth);
     }
-    console.log("arrayLimiter" , arrayLimiter);
-
-
 
     for (var i = 0; i < arrayLeng; i++) {
       for (var j = 0; j < epocArray.length + 1; j++) {
         if (epocArray[j] == arrayLimiter[i] || Math.abs((epocArray[j] - arrayLimiter[i])) < oneMonth) {
-            if (!blackList.includes(epocArray[j])) {
-              blackList.push(epocArray[j]);
-              postRate[i] = addOne;
-              addOne++;
-            }
+          if (!blackList.includes(epocArray[j])) {
+            blackList.push(epocArray[j]);
+            postRate[i] = addOne;
+            addOne++;
+          }
         }
-
       }
-
       addOne = 1;
     }
     for (var i = 0; i < postRate.length; i++) {
       if (!postRate[i])
       postRate[i] = 0;
     }
-
-    console.log("postRate " , postRate);
+    console.log(postRate);
     return postRate;
   }
 
+  postMost(date){
+    if (this.convertDatePerMonth(date)[this.convertDatePerMonth(date).length - 1] < 4) {
+      return(<h1>Post Most</h1>);
+    }else {
+      return(<h1>Post Enough</h1>);
+    }
+  }
 
-
-calc(postsData){
+returnGraph(postsData){
     const date = postsData.map(i => i.date);
-    // const epocTime = Date.parse('01 July 1970 00:00:00 GMT');
-  //   const temp = postsData.list.map(weather => weather.main.temp);
-  //   const Pressures = postsData.list.map(weather => weather.main.pressure);
-  //   const Humidites = postsData.list.map(weather => weather.main.humidity);
-  //
-  // const Pressures = cityData.list.map(weather => weather.main.pressure);
   return(
       <div key={date}>
-        {/* {this.convertDate(date)}
-        <Sparklines height={20} width={80} data={[45, 46]}> */}
-        <Sparklines height={20} width={80} data={this.convertDate(date)}>
+        <Sparklines height={20} width={40} data={this.convertDatePerMonth(date)}>
             <SparklinesLine color={'red'}/>
-            {/* <SparklinesReferenceLine type="mean" /> */}
             <SparklinesReferenceLine type="avg" />
         </Sparklines>
-        <div> avg </div>
-        <h1>{date}</h1>
-        <h1>Date </h1>
+        <h1>Post Rate {this.convertDatePerMonth(date)[this.convertDatePerMonth(date).length - 1]}</h1>
+        <h1>{this.convertDatePerMonth(date).length}</h1>
+        {this.postMost(date)}
       </div>
     );
   }
@@ -91,8 +82,7 @@ calc(postsData){
   render() {
     return(
       <div>
-        {console.log(this.props.posts)}
-        {this.props.posts.map(this.calc)}
+        {this.props.posts.map(this.returnGraph)}
     </div>
     );
   }
